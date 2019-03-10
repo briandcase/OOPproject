1082e828a900bd1219a65cc3747013c61932ce23
brian <bdcase@gmail.com>
2019-03-10 09:57:22 -0500

    add medication, and driver classes

---
+++ src/Medication.java
@@ -0,0 +1,31 @@
+public class Medication
+{
+    private String name;
+    private double price;
+
+    public Medication(String n, double p)
+    {
+        this.name = n;
+        this.price = p;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setName(String n)
+    {
+        this.name = n;
+    }
+
+    public void setPrice(double p)
+    {
+        this.price = p;
+    }
+}
