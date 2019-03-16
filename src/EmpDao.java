bb7621e1b1bf0c2e745667652db7e24584b8a4a4
Brian Case <bdcase@gmail.com>
2019-03-16 13:11:50 -0500

    initial nurse

---
+++ src/EmpDao.java
@@ -0,0 +1,129 @@
+import java.util.*;  
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;  
+  
+public class EmpDao {  
+  
+    public static Connection getConnection(){  
+        Connection con=null;  
+        try {  
+            String dbURL = "jdbc:sqlserver://MYPC\\SQLEXPRESS;databaseName=NAK_Hospital";
+            String user = "root";
+            String pass = "secret";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(dbURL, user, pass);
+            if (conn != null) 
+            {
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+            }  
+        }  catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }  
+        return con;  
+    }  
+    public static int save(Emp e){  
+        int status=0;  
+        try{  
+            Connection con=EmpDao.getConnection();  
+            PreparedStatement ps=con.prepareStatement(  
+                         "insert into user905(name,password,email,country) values (?,?,?,?)");  
+            ps.setString(1,e.getName());  
+            ps.setString(2,e.getPassword());  
+            ps.setString(3,e.getEmail());  
+            ps.setString(4,e.getCountry());  
+              
+            status=ps.executeUpdate();  
+              
+            con.close();  
+        }catch(Exception ex){ex.printStackTrace();}  
+          
+        return status;  
+    }  
+    public static int update(Emp e){  
+        int status=0;  
+        try{  
+            Connection con=EmpDao.getConnection();  
+            PreparedStatement ps=con.prepareStatement(  
+                         "update user905 set name=?,password=?,email=?,country=? where id=?");  
+            ps.setString(1,e.getName());  
+            ps.setString(2,e.getPassword());  
+            ps.setString(3,e.getEmail());  
+            ps.setString(4,e.getCountry());  
+            ps.setInt(5,e.getId());  
+              
+            status=ps.executeUpdate();  
+              
+            con.close();  
+        }catch(Exception ex){ex.printStackTrace();}  
+          
+        return status;  
+    }  
+    public static int delete(int id){  
+        int status=0;  
+        try{  
+            Connection con=EmpDao.getConnection();  
+            PreparedStatement ps=con.prepareStatement("delete from user905 where id=?");  
+            ps.setInt(1,id);  
+            status=ps.executeUpdate();  
+              
+            con.close();  
+        }catch(Exception e){e.printStackTrace();}  
+          
+        return status;  
+    }  
+    public static Emp getEmployeeById(int id){  
+        Emp e=new Emp();  
+          
+        try{  
+            Connection con=EmpDao.getConnection();  
+            PreparedStatement ps=con.prepareStatement("select * from user905 where id=?");  
+            ps.setInt(1,id);  
+            ResultSet rs=ps.executeQuery();  
+            if(rs.next()){  
+                e.setId(rs.getInt(1));  
+                e.setName(rs.getString(2));  
+                e.setPassword(rs.getString(3));  
+                e.setEmail(rs.getString(4));  
+                e.setCountry(rs.getString(5));  
+            }  
+            con.close();  
+        }catch(Exception ex){ex.printStackTrace();}  
+          
+        return e;  
+    }  
+    public static List<Emp> getAllEmployees(){  
+        List<Emp> list=new ArrayList<Emp>();  
+          
+        try{  
+            Connection con=EmpDao.getConnection();  
+            PreparedStatement ps=con.prepareStatement("select * from user905");  
+            ResultSet rs=ps.executeQuery();  
+            while(rs.next()){  
+                Emp e=new Emp();  
+                e.setId(rs.getInt(1));  
+                e.setName(rs.getString(2));  
+                e.setPassword(rs.getString(3));  
+                e.setEmail(rs.getString(4));  
+                e.setCountry(rs.getString(5));  
+                list.add(e);  
+            }  
+            con.close();  
+        }catch(Exception e){e.printStackTrace();}  
+          
+        return list;  
+    }  
+}  
